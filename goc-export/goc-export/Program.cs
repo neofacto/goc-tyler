2148a5688640e3593a75edb21282e74288b46e0e
Christopher Robert <christopher.robert@neofacto.com>
2019-03-09 01:03:21 +0100

    wip no space left

---
+++ goc-export/goc-export/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+
+namespace goc_export
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //fetch list of localities + coordinates
+            var localityFetcher = new LocalityFetcher();
+            var cityList = localityFetcher.GetLocalitiesFromFile();
+
+
+            foreach(var f in Directory.GetFiles("../../../export/export/"))
+            {
+
+            }
+            //loop on each file
+            foreach (var d in Directory.GetDirectories("../../../export/export/"))
+            {
+
+            }
+            //foreach file we search the content for one of the listed localities
+            //if we have a match we create a new result 
+
+            Console.ReadLine();
+        }
+    }
+}
